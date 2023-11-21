e875e7903b11bfdae3059f924b77e9ac503dfff3
deividsonsabino <deividsonsabino@outlook.com.br>
2023-11-20 23:02:45 -0300

    🚶🏽feat: player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed = 8.0f;
 
+    private Vector2 moveDirection;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        rb.AddForce(new Vector3(moveDirection.x, 0, moveDirection.y) * moveSpeed);
+    }
+
+    private void movePlayer(Vector3 direction) { 
+        rb.velocity = direction * moveSpeed * Time.fixedDeltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        movePlayer(moveDirection);
+    }
 }
